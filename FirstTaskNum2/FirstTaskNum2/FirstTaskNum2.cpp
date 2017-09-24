f415bb3a2ce0c1e48e890b52b7b2e8ee7f1e2782
TemaSolovei <temasolovei@gmail.com>
2017-09-25 00:32:21 +0500

    Добавлены комментарии и описание задачи

--- FirstTaskNum2/FirstTaskNum2/FirstTaskNum2.cpp
+++ FirstTaskNum2/FirstTaskNum2/FirstTaskNum2.cpp
@@ -6,8 +6,13 @@
 #include <Windows.h>
 using namespace std;
 
+/// Напишите программу, которая выводит на экран значение скалярного произведения двух векторов, 
+/// значения элементов которых вводятся с клавиатуры. Для ввода значений элементов массива
+/// напишите процедуру. Вычисление скалярного произведения реализуется как 
+/// функция (векторы передаются как параметры).
 
-int inSize()
+
+int inSize() // Получение размерности массива
 {
 	int size; // Размерность вектора
 	cout << "Введите размерность векторов (целое число): ";
@@ -16,7 +21,7 @@ int inSize()
 	return size;
 }
 
-double* inVector(int size)
+double* inVector(int size) // Ввод векторов
 {
 	double *mas = new double[size]; // Выделение памяти под массив
 
@@ -30,7 +35,7 @@ double* inVector(int size)
 	return mas;
 }
 
-double* calculate(double* masOne, double* masTwo, int size) // Вычисление произведения
+double* calculate(double* masOne, double* masTwo, int size) // Вычисление произведения векторов
 {
 	double *newMas = new double[size]; // Выделение памяти под массив
 
@@ -54,7 +59,7 @@ int main()
 	cout << "Ввод второго вектора. Вводите только числа!" << endl;
 	double *vectorTwo = inVector(size);
 
-	double *result = calculate(vectorOne, vectorTwo, size);
+	double *result = calculate(vectorOne, vectorTwo, size); // Вычисление произведения
 
 	cout << "Произведение двух векторов выглядит следующим образом:" << endl;
 	for (int i = 0; i < size; i++)
@@ -66,94 +71,3 @@ int main()
 
     return 0;
 }
-
-
-//// FirstTaskNum1.cpp: определяет точку входа для консольного приложения.
-////
-//
-//#include "stdafx.h"
-//#include <iomanip> 
-//#include <iostream>
-//#include <Windows.h>
-//using namespace std;
-//
-///// Напишите программу, которая выводит на экран значение факториала для натурального 
-///// числа, которое вводится с клавиатуры. Вычисление реализуется с помощью функции.
-///// Разработайте функцию, реализующую:
-///// а) итерационный алгоритм;
-///// б) рекурсивный алгоритм.
-//
-//double checkInt() // Проверка ввода на int
-//{
-//	double input;
-//
-//	std::cin >> input;
-//	if (!std::cin)
-//	{
-//		std::cout << "В следующий раз введите число! В дальнейшем программа будет работать некорректно" << std::endl;
-//	}
-//
-//	return input;
-//}
-//
-//double iteration(double num) // Вычисление итерационным методом
-//{
-//	double result = 1;
-//
-//	if (num < 0) return 0;
-//	else if (num == 0) return 1;
-//	else for (int i = 1; i <= num; i++)
-//	{
-//		result *= i;
-//	}
-//
-//	return result;
-//}
-//
-//double recursion(double num) // Вычислние рекурсией
-//{
-//	if (num < 0) return 0;
-//	else if (num == 0) return 1;
-//	else return num*recursion(num - 1);
-//}
-//
-//double input() // Ввод числа
-//{
-//	double num; // Введёное число
-//	cout << "Введите число для вычисления факториала: ";
-//	num = checkInt();
-//	cout << endl;
-//	return num;
-//}
-//
-//int choiceMethod() // Ввод выбранного метода для вычисления факториала
-//{
-//	int choice; // Выбранный пункт меню
-//	cout << "Как будем вычислять факториал? 1. Итерационнным алгоритмом 2. Рекурсионным алгоритмом" << endl << "Введите выбранный вариант: ";
-//	choice = checkInt();
-//	cout << endl;
-//	return choice;
-//}
-//
-//int main()
-//{
-//	SetConsoleCP(1251);       // Включаем отображение кириллицы
-//	SetConsoleOutputCP(1251); // Включаем отображение кириллицы
-//	double fact = input(); // Ввод числа
-//	double factOld = fact; // Введённое число для вывода
-//	int choice = choiceMethod();
-//
-//	switch (choice)
-//	{
-//	case 1:
-//		fact = iteration(fact);
-//		break;
-//
-//	case 2:
-//		fact = recursion(fact);
-//		break;
-//	}
-//
-//	cout << "Факториал числа " << factOld << " равен " << fact << endl;
-//	return 0;
-//}
